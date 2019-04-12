b6cd392a7e18b19ef6ee1a1ece27f8c055416580
Rahul Mahajan <aerorahul@users.noreply.github.com>
2019-04-12 10:46:17 -0400

    remove references to UCAR and copyrights
    Change-Id: Ic308c8fd539c4cee2966476425a1f4c173a71692

--- cmake/compiler_flags_Intel_C.cmake
+++ cmake/compiler_flags_Intel_C.cmake
@@ -1,8 +1,3 @@
-# (C) Copyright 2018 UCAR.
-#
-# This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-
 ####################################################################
 # FLAGS COMMON TO ALL BUILD TYPES
 ####################################################################
--- cmake/compiler_flags_XL_Fortran.cmake
+++ cmake/compiler_flags_XL_Fortran.cmake
@@ -1,9 +1,3 @@
-# (C) Copyright 2018 UCAR.
-#
-# This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-
-
 # WARNING!!
 # Following has not been thoroughly tested. Take these flags with grain of salt
 
